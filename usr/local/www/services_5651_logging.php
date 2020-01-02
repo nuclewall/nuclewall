dcd82e47be5f1137bcd99946cb2a0df88bc2f14d
Ogun Acik <acikdev@gmail.com>
2020-01-03 02:11:08 +0300

    Fix cron interval

--- usr/local/www/services_5651_logging.php
+++ usr/local/www/services_5651_logging.php
@@ -77,7 +77,7 @@ if ($_POST)
 
 		if(isset($_POST['enable']))
 		{
-			install_cron_job('/usr/local/bin/dhcp_logger', true, '0', '*/1', '*', '*', '*', 'root');
+			install_cron_job('/usr/local/bin/dhcp_logger', true, '0', '*', '*', '*', '*', 'root');
 
 			if($_POST['sign_type'] == 'local')
 			{
