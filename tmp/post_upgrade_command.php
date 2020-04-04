9f72dc4d71e7f6a0db591c0ca8ef5697a1e73151
Ogun Acik <acikogun@gmail.com>
2020-04-04 15:30:31 +0300

    Remove obsoleted 'pfSense.obsoletedfiles' system
    There is no point to use it since we use Git vcs anymore

--- tmp/post_upgrade_command.php
+++ tmp/post_upgrade_command.php
@@ -13,10 +13,4 @@
 	}
 
 	setup_serial_port();
-
-	$files_to_process = split("\n", file_get_contents("/etc/pfSense.obsoletedfiles"));
-	foreach($files_to_process as $filename)
-		if(file_exists($filename))
-			exec("/bin/rm -f $filename");
-
 ?>
